204b244f557e47107c69e3dca3f75f71006bacd8
tnglt11 <pakbafi@gmail.com>
2015-10-01 22:03:43 +0700

    Rename Member to Customer

---
+++ carrentalsystem/src/main/java/com/crs/entity/Customer.java
@@ -0,0 +1,20 @@
+package com.crs.entity;
+
+import java.util.Date;
+
+public class Customer {
+	long customerId;
+	String customerName;
+	String yearOfBird;
+	String email;
+	String phone;
+	String address;
+	String drivingLicenceNo;
+	String licenceIssuedBy;
+	Date licenceIssuedDate;
+	String identifierNumber;
+	String identifierIssuedBy;
+	Date identifierIssuedDate;
+	String userName;
+	String password;
+}
